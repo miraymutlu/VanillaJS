a5781f4cef7914f1334205b531d85495133dddfc
miraymutlu <miray.mutlu@ozu.edu.tr>
2022-06-21 22:00:44 +0300

    app.js updated

---
+++ Counter/app.js
@@ -0,0 +1,27 @@
+//initial count
+let count = 0;
+
+//select value and button
+const value = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click",function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count = 0;
+        }
+        if(count>0){
+            value.style.color="green";
+        }else if(count<0){
+            value.style.color="red";
+        }else{
+            value.style.color="#222"
+        }
+        value.textContent=count;
+    });
+});
